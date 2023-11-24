afab7a2d108665fef7271d39994b402b85c1a8b2
王志豪 <2084636055@qq.com>
2023-11-24 18:52:28 +0800

    一个学校管理后台的框架，第一次提交

---
+++ src/main/java/com/sch/smp_school/SmpSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.sch.smp_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmpSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmpSchoolApplication.class, args);
+    }
+
+}
